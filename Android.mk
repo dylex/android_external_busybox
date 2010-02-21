3dafeff1e37f7b7b38c8968380de092516b46925
Koushik K. Dutta <koushd@gmail.com>
2010-02-21 01:05:11 -0800

    fix up name to match convention

--- Android.mk
+++ Android.mk
@@ -1,5 +1,4 @@
-busybox_local_path := $(call my-dir)
-LOCAL_PATH := $(busybox_local_path)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 KERNEL_MODULES_DIR?=/system/modules/lib/modules
@@ -51,7 +50,6 @@ ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
 
 # Make a static library for clearsilver's regex. This prevent multiple
 # symbol definition error.
-LOCAL_PATH := $(busybox_local_path)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ../clearsilver/util/regex/regex.c
 LOCAL_MODULE := libclearsilverregex
@@ -61,9 +59,10 @@ LOCAL_C_INCLUDES := \
 
 include $(BUILD_STATIC_LIBRARY)
 
+
 # Build a static busybox for the recovery image
-LOCAL_PATH := $(busybox_local_path)
 include $(CLEAR_VARS)
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 
 KERNEL_MODULES_DIR?=/system/modules/lib/modules
 
@@ -86,9 +85,12 @@ LOCAL_CFLAGS := \
 	-D'CONFIG_DEFAULT_MODULES_DIR="$(KERNEL_MODULES_DIR)"' \
 	-D'BB_VER="$(strip $(shell make -s -C $(LOCAL_PATH) kernelversion))"' -DBB_BT=AUTOCONF_TIMESTAMP
 
-LOCAL_MODULE := recoverybusybox
+LOCAL_MODULE := recovery_busybox
 LOCAL_MODULE_STEM := busybox
 LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 LOCAL_STATIC_LIBRARIES += libc libm libclearsilverregex
+ADDITIONAL_RECOVERY_EXECUTABLES += recovery_busybox
 
 include $(BUILD_EXECUTABLE)
