f532e29c9e5a29817c2f908238b5049688af83c3
Koushik Dutta <koushd@gmail.com>
2010-06-26 22:06:01 -0700

    fixes to make busybox work on stock froyo bionic

--- Android.mk
+++ Android.mk
@@ -17,6 +17,16 @@ KERNEL_MODULES_DIR?=/system/modules/lib/modules
 BUSYBOX_SRC_FILES = $(shell cat $(LOCAL_PATH)/busybox-$(BUSYBOX_CONFIG).sources) \
 	libbb/android.c
 
+ifeq ($(TARGET_ARCH),arm)
+  BUSYBOX_SRC_FILES += \
+    android/libc/arch-arm/syscalls/adjtimex.S \
+    android/libc/arch-arm/syscalls/getsid.S \
+    android/libc/arch-arm/syscalls/stime.S \
+    android/libc/arch-arm/syscalls/swapon.S \
+    android/libc/arch-arm/syscalls/swapoff.S \
+    android/libc/arch-arm/syscalls/sysinfo.S
+endif
+
 BUSYBOX_C_INCLUDES = \
 	$(LOCAL_PATH)/include-$(BUSYBOX_CONFIG) \
 	$(LOCAL_PATH)/include $(LOCAL_PATH)/libbb \
